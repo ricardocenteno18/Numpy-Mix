62493649e451ee728fc41fb9526535e5d7012b99
ricardocenteno18 <61193919+ricardocenteno18@users.noreply.github.com>
2020-08-07 12:05:30 -0500

    Add files via upload

---
+++ numpy excersice.py
@@ -0,0 +1,19 @@
+import numpy as np 
+letters =  np.array([1, 3, 5, 7, 9, 7, 5])
+
+#3rd to 5th element
+print(letters[2:5])
+
+#1st to 4th element
+print(letters[:-5])
+
+#6th to the last element
+print(letters[5:])
+
+#1st to the last element
+print(letters[:])
+
+#reverse way
+print(letters[::-5])
+
+
